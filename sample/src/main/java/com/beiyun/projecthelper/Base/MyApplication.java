280d666277d49b6aff19f697678e6e0b2fc709fb
beiyun <alone169@163.com>
2017-11-07 16:50:31 +0800

    add some code

--- sample/src/main/java/com/beiyun/projecthelper/Base/MyApplication.java
+++ sample/src/main/java/com/beiyun/projecthelper/Base/MyApplication.java
@@ -1,15 +1,14 @@
 package com.beiyun.projecthelper.Base;
 
-import android.app.Activity;
 import android.app.Application;
-import android.os.Bundle;
 
 import com.beiyun.library.base.ProjectHelper;
 
 /**
  * Created by beiyun on 2017/11/1.
+ *
  */
-public class MyApplication extends Application implements Application.ActivityLifecycleCallbacks {
+public class MyApplication extends Application {
 
     @Override
     public void onCreate() {
@@ -17,38 +16,4 @@ public class MyApplication extends Application implements Application.ActivityLi
         ProjectHelper.init(this);
     }
 
-    @Override
-    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
-
-    }
-
-    @Override
-    public void onActivityStarted(Activity activity) {
-
-    }
-
-    @Override
-    public void onActivityResumed(Activity activity) {
-
-    }
-
-    @Override
-    public void onActivityPaused(Activity activity) {
-
-    }
-
-    @Override
-    public void onActivityStopped(Activity activity) {
-
-    }
-
-    @Override
-    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
-
-    }
-
-    @Override
-    public void onActivityDestroyed(Activity activity) {
-
-    }
 }
